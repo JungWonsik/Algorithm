2875eee6ac9a0889cc5bee020d3f01a22bbad42f
JungWonsik <ws_45@naver.com>
2023-03-21 15:46:23 +0900

    [Silver I] Title: 하노이 탑, Time: 532 ms, Memory: 65696 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		BigInteger[] arr = new BigInteger[101];
+		arr[1] = BigInteger.ONE;
+		for (int i = 2; i <= 100; i++)
+			arr[i] = arr[i-1].multiply(BigInteger.TWO).add(BigInteger.ONE);
+		sb.append(arr[N].toString()+"\n");
+		if (N <= 20) 
+			Hanoi(N, 1, 3);
+		System.out.println(sb);
+	}
+	
+	private static void Hanoi(int n, int start, int end) {
+		int mid = 6 - start - end;
+		if (n == 1) {
+			sb.append(start+" "+end+"\n");
+			return;
+		} 
+		Hanoi(n-1, start, mid); 
+		Hanoi(1, start, end);
+		Hanoi(n-1, mid, end);
+		return;
+	}
+}
