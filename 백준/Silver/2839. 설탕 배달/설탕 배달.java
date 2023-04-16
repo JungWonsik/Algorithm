923d236e88beefc9337ca6907fb4c7b3d8d965ce
JungWonsik <ws_45@naver.com>
2023-04-16 18:54:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 116 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,43 +1,50 @@
 import java.util.Scanner;
 
+public class Main {	
+	
+	static int N;
+	
+	public static void main(String[] args) {
 
-public class Main {
-    public static void main(String[] args) {
-    	
-    
-    	Scanner sc = new Scanner(System.in);
-    	
-    	int sugar = sc.nextInt();	 //처음 설탕무
-    	
-    	int bag = 0;
-    	
-    	
-    	while(true) {
-    		
-    		if(sugar % 5 == 0) {
-    			bag += sugar / 5;
-    			System.out.println(bag);
-    			break;
-    			
-    		} else {
-    			sugar -= 3;
-    			bag++;
-    		}	
-    		if(sugar<0) {
-    			System.out.println(-1);
-    			break;
-    		}
-    		
-    		
-    	}
-    	
-    	
-    	
-    	
-
-
-    	
-    } //
-} //
-
-
+			Scanner sc = new Scanner(System.in);
+			
+			N = sc.nextInt();
+			int cnt = 0;
+			
+			if(N%5 == 0) {
+				cnt = N / 5;
+			} else {
+				
+				while( true ) {
+					
+					if(N-3 >= 0 && N%5 != 0 ) {
+						N = N-3;
+						cnt ++;
+					}
+	 				
+					if( N % 5 == 0 ) {
+						cnt += N / 5;
+						break;
+					}
+					
+					if(N - 3 < 0) {
+						cnt = -1;
+						break;
+					}
+					
+				}
+				
+			} 
+		
+			if(cnt < 0 ) {
+				System.out.println(-1);
+			} else {
+				System.out.println(cnt);
+			}
+				
+		
+		
+		
+		
+		}
+}
